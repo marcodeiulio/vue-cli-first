07c1eb6e1e57e10f99a706d947d09bebfdcef1bc
marcodeiulio <mdeiulio@icloud.com>
2022-02-07 17:26:43 +0100

    - Added Hero.vue - Moved Vue icon from App.vue to Hero.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <Hero />
     <Title />
   </div>
 </template>
 
 <script>
 import Title from "./components/Title.vue";
+import Hero from "./components/Hero.vue";
 
 export default {
   name: "App",
   components: {
     Title,
+    Hero,
   },
 };
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,12 @@
+<template>
+  <section id="jumbotron">
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
