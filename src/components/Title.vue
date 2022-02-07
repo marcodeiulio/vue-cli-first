dd61ffce51e84814debc2d4a2fde2a44b3977587
marcodeiulio <mdeiulio@icloud.com>
2022-02-07 17:18:51 +0100

    - Added Title.vue - Removed HelloWorld.vue

---
+++ src/components/Title.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+  name: "title",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: seagreen;
+}
+</style>
